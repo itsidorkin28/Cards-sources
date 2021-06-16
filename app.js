d3db3b82cb12eabb21b9f9333b10d8e89c0225e5
Sidorkin <itsidorkin@yandex.ru>
2021-06-16 11:53:53 +0300

    commit

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const slides = document.querySelectorAll('.slide')
+let slidesPlugin = (activeSlide = 0) => {
+    const slides = document.querySelectorAll('.slide')
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')
-    })
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+
+    slides[activeSlide].classList.add('active')
+
+    // let clearActiveClasses = () => {
+    //     for (const slide of slides) {
+    //         slide.classList.remove('active')
+    //     }
+    // }
+
+    let clearActiveClasses = () => {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
 
-// let clearActiveClasses = () => {
-//     for (const slide of slides) {
-//         slide.classList.remove('active')
-//     }
-// }
-
-let clearActiveClasses = () => {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+slidesPlugin()
